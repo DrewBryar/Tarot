a60dcb6852e9ff17b666212ec8ffd508a9918b60
unknown <arielbpang@gmail.com>
2019-11-20 15:05:02 -0600

    add context

--- apps/tarot_app/views.py
+++ apps/tarot_app/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return render(request, "tarot_app/tarot.html")
+
+def main(request):
+    context ={
+        'butts': 'very yes'
+    }
+    pass
