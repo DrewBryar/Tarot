368345372de0a29885fb53e55465438d96d635c4
unknown <arielbpang@gmail.com>
2019-11-21 17:34:20 -0600

    ok

--- apps/tarot_app/urls.py
+++ apps/tarot_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^tarot$', views.tarot),
-    url(r'^tarot/questionaire$', views.tarot_question)
+    url(r'^tarot/questionaire$', views.tarot_question),
     url(r'^user/register$', views.register),
     url(r'^user/login$', views.login),
     url(r'^user/logout$', views.logout)
